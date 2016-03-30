9ff6246cc76f481aef0602763817c5129241bbb4
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2016-03-30 15:06:11 +0100

    Split routes into sections

--- app/routes.js
+++ app/routes.js
@@ -2,43 +2,55 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  
   res.render('index');
-
 });
 
-
-// Example routes - feel free to delete these
-
-// Passing data into a page
-
-router.get('/examples/template-data', function (req, res) {
-
-  res.render('examples/template-data', { 'name' : 'Foo' });
-
+router.get('/alpha-beta-banners/', function (req, res) {
+  res.render('elements/alpha-beta-banners');
 });
 
-// Branching
-
-router.get('/examples/over-18', function (req, res) {
-
-  // get the answer from the query string (eg. ?over18=false)
-  var over18 = req.query.over18;
-
-  if (over18 == "false"){
-
-    // redirect to the relevant page
-    res.redirect("/examples/under-18");
-
-  } else {
-
-    // if over18 is any other value (or is missing) render the page requested
-    res.render('examples/over-18');
-
-  }
-
+router.get('/buttons/', function (req, res) {
+  res.render('elements/buttons');
 });
 
-// add your routes here
+router.get('/colour/', function (req, res) {
+  res.render('elements/colour');
+});
+
+router.get('/data/', function (req, res) {
+  res.render('elements/data');
+});
+
+router.get('/errors/', function (req, res) {
+  res.render('elements/errors');
+});
+
+router.get('/footer/', function (req, res) {
+  res.render('elements/footer');
+});
+
+router.get('/form-elements/', function (req, res) {
+  res.render('elements/form-elements');
+});
+
+router.get('/header/', function (req, res) {
+  res.render('elements/header');
+});
+
+router.get('/icons-images/', function (req, res) {
+  res.render('elements/icons-images');
+});
+
+router.get('/layout/', function (req, res) {
+  res.render('elements/layout');
+});
+
+router.get('/naming-services/', function (req, res) {
+  res.render('elements/naming-services');
+});
+
+router.get('/typography/', function (req, res) {
+  res.render('elements/typography');
+});
 
 module.exports = router;
